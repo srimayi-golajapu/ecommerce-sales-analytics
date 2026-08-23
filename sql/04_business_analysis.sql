23f7cc11e6246251efc92df735e013c985089577
Srimayi Golajapu <srimayiofficial@gmail.com>
2026-08-23 12:32:49 +0530

    Delete sql directory

--- sql/04_business_analysis.sql
+++
@@ -1,8 +0,0 @@
--- ==========================================================
--- Step 1: Total Revenue
--- ==========================================================
-
-SELECT
-    SUM(quantity * price) AS total_revenue
-FROM ecommerce_sales_classified
-WHERE transaction_type = 'Sale';
